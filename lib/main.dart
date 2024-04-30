fbb9f2b6ba611a9dce17290f79cd2f6b4e2a397b
mahmoud hassan <mahmoudhs236@gmail.com>
2024-04-30 03:09:17 +0300

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
-import 'package:ecommerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
+// Todo: Add Widgets Binding
+// Todo: Init Local Storage
+// Todo: Await Native Splash
+// Todo: Initialize Firebase
+//Todo: Initiolize Authentication
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      debugShowCheckedModeBanner: false,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
