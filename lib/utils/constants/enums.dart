0dfa078f0d551691838b1fd968390470072fe16e
mahmoud hassan <mahmoudhs236@gmail.com>
2024-04-30 16:54:11 +0300

    updated add constants & packages
    updated add constants & packages

---
+++ lib/utils/constants/enums.dart
@@ -0,0 +1,8 @@
+//  LIST OF Enums
+// They cannot be created inside a class.
+
+enum TextSizes { small, medium, large }
+
+enum OrderStatus { processing, shipped, delivered }
+
+enum PaymentMethods { paypal, googlePay, applePay, visa, masterCard, creditCard, paystack, razorPay, paytm }
