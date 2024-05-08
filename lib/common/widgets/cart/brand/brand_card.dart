ac060cbc68342746742e6aad0bf5159a934432c4
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-09 02:21:07 +0300

    Create brand_card.dart

---
+++ lib/common/widgets/cart/brand/brand_card.dart
@@ -0,0 +1,64 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:ecommerce/common/widgets/icons/brand_title_verify_icon.dart';
+import 'package:ecommerce/common/widgets/images/circular_image.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/constants/enums.dart';
+import 'package:ecommerce/utils/constants/images.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:ecommerce/utils/helpers/helpers_functions.dart';
+import 'package:flutter/material.dart';
+
+class TBrandCard extends StatelessWidget {
+  final bool showBorder;
+  final void Function()? onTap;
+  const TBrandCard({
+    super.key, required this.showBorder,  this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: TRoundedContainer(
+        padding: const EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            // -- Image
+            Flexible(
+              child: TcircularImage(
+                image: TImages.success,
+                backgroundColor: Colors.transparent,
+                overlayColor: THelperFunctions.isDarkMode(context)
+                    ? TColors.white
+                    : TColors.black,
+              ),
+            ),
+            const SizedBox(
+              width: TSizes.spaceBtwItems / 2,
+            ),
+            // -- Text
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const TBrandTitleWithVerifiedIcon(
+                    title: 'Nike',
+                    brandTextSize: TextSizes.large,
+                  ),
+                  Text(
+                    '256 Products ProductsProductsProducts',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
