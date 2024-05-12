f1fa40feb03ed52ae07057352810287a0154e636
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-12 16:21:33 +0300

    Create choice_chip.dart

---
+++ lib/common/widgets/chips/choice_chip.dart
@@ -0,0 +1,41 @@
+import 'package:ecommerce/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/helpers/helpers_functions.dart';
+import 'package:flutter/material.dart';
+
+/// -- Most of the Styling is atready defined in the Utils -> Themes -> ChipTheme.dort
+class TChoiceChip extends StatelessWidget {
+  const TChoiceChip({
+    super.key,
+    required this.text,
+    required this.selected,
+    this.onSelected,
+  });
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
+  @override
+  Widget build(BuildContext context) {
+    final isColor = THelperFunctions.getColor(text) != null;
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor ? const SizedBox() : Text(text),
+        selected: selected,
+        onSelected: onSelected,
+        labelStyle: TextStyle(color: selected ? TColors.white : null),
+        avatar: isColor
+            ? TCircularContainer(
+                width: 50,
+                height: 50,
+                backgroundColor: THelperFunctions.getColor(text)!)
+            : null,
+        labelPadding: isColor ? const EdgeInsets.all(0) : null,
+        padding: isColor ? const EdgeInsets.all(0) : null,
+        shape: isColor ? const CircleBorder() : null,
+        backgroundColor: isColor ? THelperFunctions.getColor(text)! : null,
+      ),
+    ); // ChoiceChip
+  }
+}
