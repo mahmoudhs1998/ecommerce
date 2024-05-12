5d3f049dd62d4f86e24c506da32e7be1a0c5c136
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-12 16:10:45 +0300

    Create wishlist.dart

---
+++ lib/features/shop/screens/wishlist/wishlist.dart
@@ -0,0 +1,43 @@
+import 'package:ecommerce/common/layouts/grid_layout.dart';
+import 'package:ecommerce/common/widgets/appbar/app_bar.dart';
+import 'package:ecommerce/common/widgets/cart/product_cards/product_card_vertical.dart';
+import 'package:ecommerce/common/widgets/icons/custom_circular_icon.dart';
+import 'package:ecommerce/features/shop/screens/home/home_screen.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: TAppBar(
+        title: Text(
+          'WishList',
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
+        actions: [
+          TCircularIcon(
+            icon: Iconsax.add,
+            onPressed: () => Get.to(HomeScreen()),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              TGridLayout(
+                  itemCount: 8,
+                  itemBuilder: (_, index) => TProductCardVertical())
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
