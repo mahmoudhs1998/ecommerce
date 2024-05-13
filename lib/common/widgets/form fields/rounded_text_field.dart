ff413e087acc4249f2f54df5ea622dbf00cc573b
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-13 16:46:44 +0300

    Create rounded_text_field.dart

---
+++ lib/common/widgets/form fields/rounded_text_field.dart
@@ -0,0 +1,56 @@
+import 'package:ecommerce/utils/constants/colors.dart';
+import 'package:ecommerce/utils/helpers/helpers_functions.dart';
+import 'package:flutter/material.dart';
+
+class CustomFormField extends StatelessWidget {
+  /// `CustomFormField` is a StatelessWidget that creates a custom form field with border radius.
+  ///
+  /// This widget takes two required parameters:
+  /// - `labelText`: A String that defines the label text of the form field.
+  /// - `prefixIcon`: An IconData that defines the prefix icon of the form field.
+  ///
+  /// Usage:
+  /// ```dart
+  /// CustomFormField(
+  ///   labelText: 'Username',
+  ///   prefixIcon: Icons.person,
+  /// );
+  /// ```
+  ///
+  /// The `build` method of this widget returns a `TextFormField` with custom `InputDecoration`.
+  /// The border color of the form field changes based on the current theme mode (dark or light).
+  /// The border radius of the form field is set to 8.
+  ///
+  /// The `labelText` and `prefixIcon` passed to the `CustomFormField` are used in the `InputDecoration`.
+  // -- Custom Form Field With Border Radius
+
+  final String labelText;
+  final IconData prefixIcon;
+  const CustomFormField({
+    super.key,
+    required this.labelText,
+    required this.prefixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = THelperFunctions.isDarkMode(context);
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide( 
+              width: 0.8, color: isDark ? TColors.darkGrey : TColors.grey),
+
+          borderRadius: BorderRadius.circular(8), // Set the border radius here
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+              width: 0.8, color: isDark ? TColors.darkGrey : TColors.grey),
+          borderRadius: BorderRadius.circular(8), // Set the border radius here
+        ),
+        prefixIcon: Icon(prefixIcon),
+      ),
+    );
+  }
+}
