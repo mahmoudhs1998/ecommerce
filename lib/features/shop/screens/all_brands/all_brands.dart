6d9ba696e2c79f00f6ab70114d5208db770ca688
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-20 19:03:50 +0300

    Create all_brands.dart

---
+++ lib/features/shop/screens/all_brands/all_brands.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../../common/layouts/grid_layout.dart';
+import '../../../../common/widgets/appbar/app_bar.dart';
+import '../../../../common/widgets/cart/brand/brand_card.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
+import '../../../../utils/constants/sizes.dart';
+import 'brand_products.dart';
+
+class AllBrandsScreen extends StatelessWidget {
+  const AllBrandsScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TAppBar(title: Text('Brand'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Heading
+              const TCategoriesSectionHeading(
+                  title: 'Brands', showActionButton: false),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              /// - Brands
+              TGridLayout(
+                  itemCount: 10,
+                  mainAxisExtent: 80,
+                  itemBuilder: (context, index) => TBrandCard(
+                      showBorder: true,
+                      onTap: () => Get.to(() => const BrandProducts()))),
+            ],
+          ), // Column
+        ), // Padding
+      ), // SingleChildScrollView
+    ); // Scaffold
+  }
+}
