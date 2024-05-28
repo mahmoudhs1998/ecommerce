6bb663997f0959bd77f36028fdf4e88c243f2063
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-28 15:41:48 +0300

    Create change_name_screen.dart

---
+++ lib/features/personalization/screens/profile/widgets/change_name_screen.dart
@@ -0,0 +1,75 @@
+import 'package:ecommerce/features/personalization/controllers/update_name_controller.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../../common/widgets/appbar/app_bar.dart';
+import '../../../../../utils/constants/texts.dart';
+import '../../../../../utils/validators/validation.dart';
+
+class ChangeNameScreen extends StatelessWidget {
+  const ChangeNameScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(UpdateNameController());
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text('Change Name',
+            style: Theme.of(context).textTheme.headlineSmall),
+      ), // TAppBar
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Headings
+            Text(
+              'Use real name for easy verification. This name will appear on several pages.',
+              style: Theme.of(context).textTheme.labelMedium,
+            ), // Text
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            /// Text field and Button
+            Form(
+                key: controller.updateUserNameFormKey,
+                child: Column(children: [
+                  TextFormField(
+                    controller: controller.firstName,
+                    validator: (value) =>
+                        TValidator.validateEmptyText('First Name', value),
+                    expands: false,
+                    decoration: const InputDecoration(
+                        labelText: TTexts.firstName,
+                        prefixIcon: Icon(Iconsax.user)),
+                  ), // TextFormField
+                  const SizedBox(height: TSizes.spaceBtwInputFields),
+                  TextFormField(
+                    controller: controller.lastName,
+                    validator: (value) =>
+                        TValidator.validateEmptyText('Last Name', value),
+                    expands: false,
+                    decoration: const InputDecoration(
+                        labelText: TTexts.lastName,
+                        prefixIcon: Icon(Iconsax.user)),
+                  ),
+                ] // TextFormField
+
+                    )), // Column, Form
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            /// Save Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                  onPressed: () => controller.updateUserName(),
+                  child: const Text('Save')),
+            ), //
+          ],
+        ),
+      ),
+    );
+  }
+}
