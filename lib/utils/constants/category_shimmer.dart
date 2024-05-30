ee54006e32a7ac351804798fe6b2078ebf8d2de9
mahmoud hassan <mahmoudhs236@gmail.com>
2024-05-30 14:50:22 +0300

    Create category_shimmer.dart

---
+++ lib/utils/constants/category_shimmer.dart
@@ -0,0 +1,37 @@
+import 'package:ecommerce/utils/constants/shimmer.dart';
+import 'package:ecommerce/utils/constants/shimmer.dart';
+import 'package:ecommerce/utils/constants/sizes.dart';
+import 'package:flutter/cupertino.dart';
+
+class TCategoryShimmer extends StatelessWidget {
+  final int itemCount;
+
+  const TCategoryShimmer({
+    super.key,
+    this.itemCount = 6,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 80,
+      child: ListView.separated(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: itemCount,
+        separatorBuilder: (_, __) =>
+            const SizedBox(width: TSizes.spaceBtwItems),
+        itemBuilder: (_, __) => const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Image
+              TShimmerEffect(width: 55, height: 55, radius: 55),
+              SizedBox(height: TSizes.spaceBtwItems / 2),
+
+              /// Text
+              TShimmerEffect(width: 55, height: 8),
+            ]),
+      ),
+    );
+  }
+}
