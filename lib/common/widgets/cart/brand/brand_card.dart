3cf1971b687f4f579f53aea183c3fd96ca769361
mahmoud hassan <mahmoudhs236@gmail.com>
2024-06-08 15:47:14 +0300

    Updated
    Fetch Category-Specific Brands and their Products Using Future Builder

--- lib/common/widgets/cart/brand/brand_card.dart
+++ lib/common/widgets/cart/brand/brand_card.dart
@@ -1,6 +1,7 @@
 import 'package:ecommerce/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:ecommerce/common/widgets/icons/brand_title_verify_icon.dart';
 import 'package:ecommerce/common/widgets/images/circular_image.dart';
+import 'package:ecommerce/features/shop/models/brand_model.dart';
 import 'package:ecommerce/utils/constants/colors.dart';
 import 'package:ecommerce/utils/constants/enums.dart';
 import 'package:ecommerce/utils/constants/images.dart';
@@ -9,10 +10,11 @@ import 'package:ecommerce/utils/helpers/helpers_functions.dart';
 import 'package:flutter/material.dart';
 
 class TBrandCard extends StatelessWidget {
+  final BrandModel brand;
   final bool showBorder;
   final void Function()? onTap;
   const TBrandCard({
-    super.key, required this.showBorder,  this.onTap,
+    super.key, required this.showBorder,  this.onTap, required this.brand,
   });
 
   @override
@@ -28,7 +30,8 @@ class TBrandCard extends StatelessWidget {
             // -- Image
             Flexible(
               child: TcircularImage(
-                image: TImages.success,
+                isNetworkImage: true,
+                image:  brand.image, // TImages.success,
                 backgroundColor: Colors.transparent,
                 overlayColor: THelperFunctions.isDarkMode(context)
                     ? TColors.white
@@ -44,12 +47,12 @@ class TBrandCard extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const TBrandTitleWithVerifiedIcon(
-                    title: 'Nike',
+                   TBrandTitleWithVerifiedIcon(
+                    title: brand.name, //'Nike',
                     brandTextSize: TextSizes.large,
                   ),
                   Text(
-                    '256 Products ProductsProductsProducts',
+                     '${brand.productsCount ?? 0} Products', //'256 Products ProductsProductsProducts',
                     overflow: TextOverflow.ellipsis,
                     style: Theme.of(context).textTheme.labelMedium,
                   ),
--- lib/data/repositories/product/product_repository.dart
+++ lib/data/repositories/product/product_repository.dart
@@ -83,6 +83,32 @@ class ProductRepository extends GetxController {
 
 
 
+
+  Future<List<ProductModel>> getProductsForBrand({required String brandId, int limit = -1}) async {
+    try {
+      final querySnapshot = limit == -1
+          ? await _db.collection('Products')
+          .where('Brand.Id', isEqualTo: brandId)
+          .get()
+          : await _db.collection('Products')
+          .where('Brand.Id', isEqualTo: brandId)
+          .limit(limit)
+          .get();
+
+      final products = querySnapshot.docs.map((doc) =>
+          ProductModel.fromSnapshot(doc)).toList();
+
+      return products;
+
+    } on FirebaseException catch (e) {
+    throw TFirebaseException(e.code).message;
+    } on PlatformException catch (e) {
+    throw TPlatformException(e.code).message;
+    } catch (e) {
+    throw 'Something went wrong. Please try again';
+    }
+  }
+
   // Upload dummy data to the Cloud Firebase
   Future<void> uploadDummyData(List<ProductModel> products) async {
     try {
@@ -144,4 +170,8 @@ class ProductRepository extends GetxController {
       throw e.toString();
     }
   }
+
+
+
+
 }
