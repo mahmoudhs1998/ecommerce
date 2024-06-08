b7f9d9d2b46bd639cbb09cdf29944445608654e0
mahmoud hassan <mahmoudhs236@gmail.com>
2024-06-08 15:45:47 +0300

    Create brand_controller.dart

---
+++ lib/features/shop/controllers/Brands/brand_controller.dart
@@ -0,0 +1,55 @@
+import 'package:ecommerce/data/repositories/Brand/brand_repository.dart';
+import 'package:ecommerce/utils/popups/loaders.dart';
+import 'package:get/get.dart';
+
+import '../../../../data/repositories/product/product_repository.dart';
+import '../../models/brand_model.dart';
+import '../../models/product_model.dart';
+
+class BrandController extends GetxController
+{
+  static BrandController get instance => Get.find();
+  RxBool isLoading = true.obs;
+  final RxList<BrandModel> allBrands = <BrandModel>[].obs;
+
+  final RxList<BrandModel> featuredBrands = <BrandModel>[].obs;
+
+  final brandRepository = Get.put(BrandRepository()) ;
+
+  @override
+  void onInit() {
+    getFeaturedBrands();
+    super.onInit();
+  }
+
+/// -- Load Brands
+Future<void> getFeaturedBrands() async {
+    try {
+      // show Loader While Loading the Brands
+      isLoading.value = true;
+
+      final brands = await brandRepository.getAllBrands();
+      allBrands.assignAll(brands);
+      featuredBrands.assignAll(allBrands.where((brand) => brand.isFeatured ?? false).take(4));
+    }catch(e) {
+      TLoaders.errorSnackBar(title: 'Oh Snap!', message: e.toString());
+    }finally{
+      // Stop the Loader
+      isLoading.value = false;
+    }
+}
+
+/// -- Get Brands For Category
+
+/// Get Brand Specific Products from your data source
+  /// Get Brand Specific Products from your data source
+  Future<List<ProductModel>> getBrandProducts(String brandId) async {
+    try {
+      final products = await ProductRepository.instance.getProductsForBrand(brandId: brandId);
+      return products;
+    } catch (e) {
+      TLoaders.errorSnackBar(title: '0h Snap!', message: e.toString());
+      return [];
+    }
+  }
+}
